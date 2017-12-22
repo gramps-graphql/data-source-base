962db75d7098e71ac38de5a178b0ca055a0ef3e5
Jason Lengstorf <jason@lengstorf.com>
2017-12-22 14:37:59 -0800

    fix: update placeholder to match pattern

--- src/context.js
+++ src/context.js
@@ -2,7 +2,7 @@
 export default {
   getById: id => ({
     id,
-    name: 'GrAMPS Data Source Base',
+    name: 'GrAMPS GraphQL Data Source Base',
     lucky_numbers: [1, 2, 3, 5, 8, 13, 21],
   }),
 };
