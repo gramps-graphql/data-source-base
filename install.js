009c0e210c196207ec34c54b667bbe2dbf7be151
Jason Lengstorf <jason@lengstorf.com>
2017-12-22 14:19:46 -0800

    feat: add prompts to update files

--- install.js
+++ install.js
@@ -5,6 +5,7 @@
  * @see https://github.com/graphql-cli/graphql-cli
  */
 const fs = require('fs');
+const globby = require('globby');
 
 function replaceInFile(filePath, searchValue, replaceValue) {
   const contents = fs.readFileSync(filePath, 'utf8');
@@ -15,29 +16,55 @@ function replaceInFile(filePath, searchValue, replaceValue) {
   fs.writeFileSync(filePath, newContents);
 }
 
-module.exports = async ({ project, context }) => {
-  const opts = await context.prompt({
-    type: 'input',
-    name: 'test',
-    message: 'Do prompts work in the install script?',
+function replaceDefaultStringsInFiles(
+  { namespace, prefix, templateName, project },
+  fileGlob = '{src,test}/**/*',
+) {
+  const sourceFiles = globby.sync(fileGlob);
+  const newType = `${prefix}_${namespace}`;
+  const placeholder = `GrAMPS Data Source: ${namespace}`;
+
+  sourceFiles.forEach(filePath => {
+    replaceInFile(filePath, templateName, project);
+    replaceInFile(filePath, 'PFX_DataSourceBase', newType);
+    replaceInFile(filePath, 'DataSourceBase', namespace);
+    replaceInFile(filePath, 'GrAMPS GraphQL Data Source Base', placeholder);
   });
+}
 
-  console.log(opts);
+const questions = [
+  {
+    type: 'input',
+    name: 'namespace',
+    message: 'Choose a unique namespace. Only letters and numbers are allowed.',
+    default: 'MyDataSource',
+    validate: input => !input.match(/\W/),
+  },
+  {
+    type: 'input',
+    name: 'prefix',
+    message: 'Choose a short, unique prefix for your types.',
+    default: 'PFX',
+  },
+];
 
+module.exports = async ({ project, context }) => {
+  const opts = await context.prompt(questions);
   const templateName = '@gramps/data-source-base';
-  replaceInFile('package.json', templateName, project);
 
-  console.log(`\
-💥 You’re all set: a new GrAMPS data source has been created!
+  replaceDefaultStringsInFiles({ ...opts, templateName, project });
 
-✅ Next steps:
-  1. Change directory: \`cd ${project}\`
-  2. Start local server: \`yarn dev\`
-  3. Open the GraphQL Playground: http://localhost:8080/playground
+  console.log(`
 
-💡 Don’t forget to update the following fields in \`package.json\`:
-  -  description
-  -  contributors
-  -  repository
+💥  You’re all set: a new GrAMPS data source has been created!
+
+💡  Don’t forget to update "description", "contributors", and
+   "repository" in \`package.json\` with your own information.
+
+✅  Next steps:
+   1. Change directory: \`cd ${project}\`
+   2. Start local server: \`yarn dev\`
+   3. Open the GraphQL Playground: http://localhost:8080/playground
+   4. [BONUS] Set up Code Climate: http://bit.ly/2l0mvp9
   `);
 };
